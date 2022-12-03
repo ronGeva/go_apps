c6f6f54d69a2fbffde1879838b495465331a8636
Ron Geva <coolron54@gmail.com>
2022-12-03 20:24:11 +0200

    condition implementation
    Added go_db/condition.go, which implements logic required to run a set
    of conditions on a single record.
    This set of condition is represented the conditionNode struct, which
    includes an operator and two optional operands.
    When "checkAllConditions" is executed against a Record we run through
    the conditionNode graph, recursively calling checkAllConditions on all
    connected nodes, and applying the appropriate operators on their
    results.

---
+++ go_db/condition.go
@@ -0,0 +1,139 @@
+package go_db
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
+
+type conditionType int8
+type conditionOperator int8
+
+const (
+	ConditionTypeEqual conditionType = iota
+	ConditionTypeLess
+	ConditionTypeGreater
+	ConditionTypeRegex
+)
+
+const (
+	ConditionOperatorNull conditionOperator = iota
+	ConditionOperatorAnd
+	ConditionOperatorOr
+	ConditionOperatorNot
+)
+
+type optionalBool struct {
+	value   bool
+	isValid bool
+}
+
+type condition struct {
+	fieldIndex     uint32
+	conditionType  conditionType
+	conditionValue []byte
+}
+
+type conditionNode struct {
+	operator  conditionOperator
+	left      *conditionNode
+	right     *conditionNode
+	condition *condition
+}
+
+var SUPPORTED_CONDITIONS = map[FieldType][]conditionType{
+	FieldTypeInt:  {ConditionTypeEqual, ConditionTypeLess, ConditionTypeGreater},
+	FieldTypeBlob: {ConditionTypeEqual},
+}
+
+var OPEATOR_FUNCS = map[conditionOperator]func(left optionalBool, right optionalBool) bool{
+	ConditionOperatorAnd: andFunc,
+	ConditionOperatorOr:  orFunc,
+	ConditionOperatorNot: notFunc,
+}
+
+var CONDITION_FUNCS = map[conditionType]func(f Field, data []byte) (bool, error){
+	ConditionTypeEqual:   checkEqual,
+	ConditionTypeLess:    checkLess,
+	ConditionTypeGreater: checkGreater,
+}
+
+func andFunc(left optionalBool, right optionalBool) bool {
+	assert(left.isValid && right.isValid, "and operator requires two operands")
+	return left.value && right.value
+}
+
+func orFunc(left optionalBool, right optionalBool) bool {
+	assert(left.isValid && right.isValid, "or operator requires two operands")
+	return left.value || right.value
+}
+
+func notFunc(left optionalBool, right optionalBool) bool {
+	assert(left.isValid && !right.isValid, "not opreator accepts exactly one operands")
+	return !left.value
+}
+
+func checkEqual(f Field, data []byte) (bool, error) {
+	switch f.getType() {
+	case FieldTypeInt:
+		intField, ok := f.(IntField)
+		assert(ok, "Failed to downcast IntField")
+		return intField.Value == int(binary.LittleEndian.Uint32(data)), nil
+	case FieldTypeBlob:
+		blobField, ok := f.(BlobField)
+		assert(ok, "Failed to downcase BlobField")
+		return bytes.Equal(blobField.Data, data), nil
+	default:
+		return false, fmt.Errorf("illegal condition")
+	}
+}
+
+func checkLess(f Field, data []byte) (bool, error) {
+	switch f.getType() {
+	case FieldTypeInt:
+		intField, ok := f.(IntField)
+		assert(ok, "Failed to downcast IntField")
+		return intField.Value < int(binary.LittleEndian.Uint32(data)), nil
+	default:
+		return false, fmt.Errorf("illegal condition")
+	}
+}
+
+func checkGreater(f Field, data []byte) (bool, error) {
+	switch f.getType() {
+	case FieldTypeInt:
+		intField, ok := f.(IntField)
+		assert(ok, "Failed to downcast IntField")
+		return intField.Value > int(binary.LittleEndian.Uint32(data)), nil
+	default:
+		return false, fmt.Errorf("illegal condition")
+	}
+}
+
+func checkCondition(c condition, record Record) bool {
+	field := record.fields[c.fieldIndex]
+	conditionFunc := CONDITION_FUNCS[c.conditionType]
+	result, err := conditionFunc(field, c.conditionValue)
+	check(err) // TODO: handle error
+	return result
+}
+
+func checkAllConditions(currentNode conditionNode, record Record) bool {
+	var leftResult optionalBool
+	var rightResult optionalBool
+	if currentNode.left == nil && currentNode.right == nil {
+		assert(currentNode.condition != nil, "Node must have either a son or a condition")
+		return checkCondition(*currentNode.condition, record)
+	}
+
+	if currentNode.left != nil {
+		leftResult.value = checkAllConditions(*currentNode.left, record)
+		leftResult.isValid = true
+	}
+	if currentNode.right != nil {
+		rightResult.value = checkAllConditions(*currentNode.right, record)
+		rightResult.isValid = true
+	}
+	assert(currentNode.operator != ConditionOperatorNull, "A node with sons must have an operator")
+	return OPEATOR_FUNCS[currentNode.operator](leftResult, rightResult)
+}
--- go_db/db_test.go
+++ go_db/db_test.go
@@ -36,3 +36,38 @@ func TestFullFlow(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestConditions(t *testing.T) {
+	record := Record{[]Field{IntField{5}, IntField{7}, BlobField{[]byte{11, 13, 25}}}}
+	node1 := conditionNode{}
+	node1.condition = &condition{0, ConditionTypeEqual, uint32ToBytes(5)}
+	// Condition should succeed
+	if !checkAllConditions(node1, record) {
+		t.Fail()
+	}
+	node2 := conditionNode{}
+	node2.condition = &condition{1, ConditionTypeEqual, uint32ToBytes(6)}
+	// Condition should fail
+	if checkAllConditions(node2, record) {
+		t.Fail()
+	}
+	node3 := conditionNode{}
+	node3.left = &node1
+	node3.right = &node2
+	node3.operator = ConditionOperatorOr
+	// Condition should succeed
+	if !checkAllConditions(node3, record) {
+		t.Fail()
+	}
+	node4 := conditionNode{}
+	node4.condition = &condition{1, ConditionTypeGreater, uint32ToBytes(6)}
+	if !checkAllConditions(node4, record) {
+		t.Fail()
+	}
+	node5 := conditionNode{}
+	node5.left = &node4
+	node5.operator = ConditionOperatorNot
+	if checkAllConditions(node5, record) {
+		t.Fail()
+	}
+}
