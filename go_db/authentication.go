85cb177743bc8928c8670212268f1a8096ed588d
Ron Geva <coolron54@gmail.com>
2024-10-21 22:11:26 +0300

    go_db: Change conn+auth scores to be more reasonable
    Since our score is better the smaller it is we made a change to how the
    scores are calculated for authentication/connection provenances.
    
    Now bigger passwords will award the user with better (smaller)
    provenances and the only part of the connection IP that will be used
    for the provenance field is the most significant byte of it.

--- go_db/authentication.go
+++ go_db/authentication.go
@@ -1,6 +1,17 @@
 package go_db
 
 func authenticationScore(authentication ProvenanceAuthentication) ProvenanceScore {
-	// TODO: improve this
-	return ProvenanceScore(len(authentication.Password))
+	// Rank password as better the longer it is, capped at rank 0 (which is the best rank).
+	// Return a score between 0 and 256.
+	const optimalLength int = 32
+	const maximalScore int = 256
+	const scorePerCharacterShort int = maximalScore / optimalLength
+
+	length := len(authentication.Password)
+	if length >= optimalLength {
+		return 0
+	}
+
+	// Each character short of 64 characters will increase the score in 4, up to 256.
+	return ProvenanceScore((optimalLength - length) * scorePerCharacterShort)
 }
